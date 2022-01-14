883b36979bf4061a99891a76dbe6e85cbf806e94
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-01-14 18:03:41 +0700

    Create layout for AddTaskActivity

--- app/src/main/java/com/test/stechoq/ui/add/AddTaskActivity.kt
+++ app/src/main/java/com/test/stechoq/ui/add/AddTaskActivity.kt
@@ -1,12 +1,19 @@
 package com.test.stechoq.ui.add
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.test.stechoq.R
+import androidx.appcompat.app.AppCompatActivity
+import com.test.stechoq.databinding.ActivityAddTaskBinding
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCancelTask.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/test/stechoq/ui/list/TaskListActivity.kt
+++ app/src/main/java/com/test/stechoq/ui/list/TaskListActivity.kt
@@ -1,9 +1,15 @@
 package com.test.stechoq.ui.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.test.stechoq.R
 import com.test.stechoq.database.DataSource
 import com.test.stechoq.databinding.ActivityTaskListBinding
+import com.test.stechoq.ui.add.AddTaskActivity
 
 class TaskListActivity : AppCompatActivity() {
 
@@ -19,4 +25,22 @@ class TaskListActivity : AppCompatActivity() {
         binding.rvTaskList.setHasFixedSize(true)
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.menu_add_task -> {
+                startActivity(Intent(this, AddTaskActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
