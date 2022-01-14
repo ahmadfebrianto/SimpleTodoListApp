6be03f4d5cbd0aca9decc59fc957f0d70d3a50ee
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-01-14 19:10:15 +0700

    Hide keyboard when clicking outside edit text

--- app/src/main/java/com/test/stechoq/ui/add/AddTaskActivity.kt
+++ app/src/main/java/com/test/stechoq/ui/add/AddTaskActivity.kt
@@ -2,7 +2,14 @@ package com.test.stechoq.ui.add
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.test.stechoq.R
 import com.test.stechoq.databinding.ActivityAddTaskBinding
+import android.app.Activity
+import android.content.Context
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -12,8 +19,27 @@ class AddTaskActivity : AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.title = getString(R.string.add_task)
+
+        binding.etTaskName.addTextChangedListener {
+            val text = binding.etTaskName.editableText
+            binding.btSaveTask.isEnabled = text.isNotEmpty()
+        }
+
         binding.btCancelTask.setOnClickListener {
             finish()
+        }
+
+        binding.rootLayout.setOnClickListener {
+            hideSoftKeyboard()
+            binding.etTaskName.clearFocus()
+        }
+
+    }
+
+    private fun Activity.hideSoftKeyboard(){
+        (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+            hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 }
