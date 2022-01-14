052024cc0193f9e8a34323835b719f859c0677c1
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-01-14 17:10:36 +0700

    Make TaskList as the main activity

--- app/src/main/java/com/test/stechoq/ui/list/TaskListActivity.kt
+++ app/src/main/java/com/test/stechoq/ui/list/TaskListActivity.kt
@@ -1,17 +1,22 @@
 package com.test.stechoq.ui.list
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.test.stechoq.R
-import com.test.stechoq.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.test.stechoq.database.DataSource
 import com.test.stechoq.databinding.ActivityTaskListBinding
 
 class TaskListActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityTaskListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task_list)
+        binding = ActivityTaskListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val taskList = DataSource.loadDummyTask()
+        binding.rvTaskList.adapter = TaskListAdapter(taskList)
+        binding.rvTaskList.setHasFixedSize(true)
 
     }
 }
